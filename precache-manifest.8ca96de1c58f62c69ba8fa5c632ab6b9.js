6fc2e951e5ea69b6316c54c74532a323d82958b6
erik1933 <eric.aghajanyan@gmail.com>
2020-08-12 11:23:00 +0400

    Updates

---
+++ precache-manifest.8ca96de1c58f62c69ba8fa5c632ab6b9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc958d1d1d5972ac1a08a9f901a9f04e",
+    "url": "/hookahMobile/index.html"
+  },
+  {
+    "revision": "5d5d17dd2ec593a6fa1a",
+    "url": "/hookahMobile/static/css/main.3b39c6d5.chunk.css"
+  },
+  {
+    "revision": "6df5c123b5cc68412791",
+    "url": "/hookahMobile/static/js/2.b94bcbf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hookahMobile/static/js/2.b94bcbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d17dd2ec593a6fa1a",
+    "url": "/hookahMobile/static/js/main.950ffb2b.chunk.js"
+  },
+  {
+    "revision": "71e2d3b7c5b10b8ec3d7",
+    "url": "/hookahMobile/static/js/runtime-main.54a76ff7.js"
+  },
+  {
+    "revision": "6f443958431c45a42dc26b96f5cf3484",
+    "url": "/hookahMobile/static/media/arrow-angle-down.6f443958.svg"
+  },
+  {
+    "revision": "8f6775acf5283293bbcbf22ba7d6f958",
+    "url": "/hookahMobile/static/media/bb45.8f6775ac.png"
+  },
+  {
+    "revision": "fc82a7a65516a745a26477e3e8aed7de",
+    "url": "/hookahMobile/static/media/dice.fc82a7a6.svg"
+  },
+  {
+    "revision": "a6df86a4a2d9c8e101a136988bbddaa2",
+    "url": "/hookahMobile/static/media/filter-icon.a6df86a4.svg"
+  },
+  {
+    "revision": "4d1a80fa94b75ae72149975124cd54e1",
+    "url": "/hookahMobile/static/media/hoocah-icon-black.4d1a80fa.svg"
+  },
+  {
+    "revision": "5964fc4de2333171f35da1b1bdbcfd76",
+    "url": "/hookahMobile/static/media/hoocah-icon-green.5964fc4d.svg"
+  },
+  {
+    "revision": "bd817ee40e03ab239380e214ed94de0f",
+    "url": "/hookahMobile/static/media/hoocah-icon-orange.bd817ee4.svg"
+  },
+  {
+    "revision": "9acdcc0ed671c3429710fc9eb5ae6064",
+    "url": "/hookahMobile/static/media/hoocah-icon-red.9acdcc0e.svg"
+  },
+  {
+    "revision": "4ec38919551ef794043826d7bfe6d767",
+    "url": "/hookahMobile/static/media/kinowell.4ec38919.png"
+  },
+  {
+    "revision": "0c446f4d89aaf3bbf94131369e78f1c6",
+    "url": "/hookahMobile/static/media/o-beach.0c446f4d.png"
+  },
+  {
+    "revision": "60a3a8dfe9980bb5940557fcf9123530",
+    "url": "/hookahMobile/static/media/s-beach.60a3a8df.png"
+  },
+  {
+    "revision": "226450b81dafefc769227f7ed84e4bd6",
+    "url": "/hookahMobile/static/media/search-icon.226450b8.svg"
+  }
+]);
